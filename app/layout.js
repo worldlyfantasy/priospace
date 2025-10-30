a4f814b09ea4dac2b49431a7c4a1729a4a5428c3
Haisen <worldlyfantasy@gmail.com>
2025-10-30 16:59:19 +0800

    Stop relying on remote Nunito font

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,5 @@
 import "./globals.css";
 
-import { Nunito } from "next/font/google";
-
-const nunito = Nunito({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  variable: "--font-nunito",
-});
-
 export const metadata = {
   title: "PrioSpace",
   description: "Focus on what matters.",
@@ -16,10 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${nunito.variable} antialiased font-nunito`}
-        suppressHydrationWarning
-      >
+      <body className="antialiased font-nunito" suppressHydrationWarning>
         {children}
       </body>
     </html>
